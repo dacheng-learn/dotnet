4c35260a65e2438758f781a115aa2be08f6e8d9d
Success Go <successgdc@gmail.com>
2021-05-30 21:37:25 +0800

    Add DirectoryExample project

---
+++ examples/FileSystem/DirectoryExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "/tmp/gdc.dir";
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+            var gdcDir = Directory.CreateDirectory(path);
+            gdcDir.CreateSubdirectory("sub");
+            // Directory not empty
+            // Directory.Delete(path);
+            Directory.Delete(path, true);
+            
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            
+            // permission problem
+            // Directory.CreateDirectory("/gdc.no.permission");
+
+            // empty path raise exception 
+            // Directory.CreateDirectory("");
+        }
+    }
+}
