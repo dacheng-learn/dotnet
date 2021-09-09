27bc872c32daf8b57c2a1edf2a00869664eb6d42
Success Go <successgdc@gmail.com>
2021-09-09 15:00:46 +0800

    Adds foreach statement code example.

---
+++ 100-foreach-example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForeachExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var apples = new List<int> { 1, 2, 3, 9, 22, 500 };
+            foreach (var apple in apples)
+            {
+                Console.WriteLine(apple);
+            }
+        }
+    }
+}
