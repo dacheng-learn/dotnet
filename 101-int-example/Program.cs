afa994e6757a9df4074fb29eafa876826b3668f9
Success Go <successgdc@gmail.com>
2021-09-10 16:00:47 +0800

    Add value type of int example.

---
+++ 101-int-example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 28;
+            Console.WriteLine($"My age is {age} years old.");
+
+            int temp;
+            bool parseOk = int.TryParse("-13", out temp);
+            Console.WriteLine($"Parse int status: {parseOk}.");
+            Console.WriteLine($"Today's temp is {temp}.");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"max int is: {max}");
+            Console.WriteLine($"min int is: {min}");
+        }
+    }
+}
