1ff3fdfa013421875c1e2b6765ac554dc2e3370c
Success Go <successgdc@gmail.com>
2021-09-17 21:18:10 +0800

    Added for loops demo.

---
+++ 103-for-demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace For.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 循环打印10次 hello world
+             */
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hello World for i = {i+1}");
+            }
+        }
+    }
+}
