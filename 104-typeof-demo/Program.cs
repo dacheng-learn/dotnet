4aef8bbec4e783fac5debd168bb75b2b762f1c03
Success Go <successgdc@gmail.com>
2021-09-25 18:30:12 +0800

    Added typeof demo.

---
+++ 104-typeof-demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Typeof.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output our custom type, Program itself.
+            Console.WriteLine(typeof(Program));
+            
+            // Output the sdk type.
+            Console.WriteLine(typeof(Console));
+            Console.WriteLine(typeof(HttpClient));
+            
+            // Output the actual type.
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(typeof(uint));
+            Console.WriteLine(typeof(long));
+            Console.WriteLine(typeof(ulong));
+            Console.WriteLine(typeof(float));
+            Console.WriteLine(typeof(double));
+            Console.WriteLine(typeof(decimal));
+            Console.WriteLine(typeof(string));
+            
+            // Output the same result.
+            Console.WriteLine(typeof(bool));
+            Console.WriteLine(typeof(Boolean));
+        }
+    }
+}
